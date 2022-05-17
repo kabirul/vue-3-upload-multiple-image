22c4cf24126f4f1b0e46174844179285e96644c8
Kabirul Islam Mondal <kabirul05@gmail.com>
2022-05-17 19:39:44 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container" style="width:600px">
+      <div style="margin:50px 0;">
+        <p id="devname"><span>Amica</span></p>
+        <h4 style="margin-top:20px">Vue.js 3 upload multiple Images</h4>
+      </div>
+      <upload-images></upload-images>
+    </div>
+	<footer-page></footer-page>
+  </div>
+</template>
+<script>
+import UploadImages from "./components/UploadImages";
+import FooterPage from './components/Footer';
+
+export default {
+  name: "App",
+  components: {
+    UploadImages,
+	FooterPage
+  }
+};
+</script>
+<style>
+#devname span{ background-color:#000000; color:#FFFFFF; width:100px; height:100px; padding:20px 10px; border-radius:50%; font-size:15px; }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p class="text-center">Vue.js 3 upload multiple images tutorial <a href="https://github.com/kabirul" target="_blank">amica.</a></p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer-page', 
+}
+</script>
+<style>
+footer { width:75%; margin:0 auto;}
+footer p { margin-top: 50px;}
+.text-center { text-align:center;}
+</style>
